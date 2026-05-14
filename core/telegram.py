e4d75af11016e542f54020d1fb0baa43656b5c80
Jonomuzz <156991284+Jonomuzz@users.noreply.github.com>
2026-05-14 19:31:03 +1000

    Change chat ID variable and update message sending

--- core/telegram.py
+++ core/telegram.py
@@ -1,25 +1,22 @@
 import requests
 
-from core.config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+from core.config import TELEGRAM_TOKEN, ALERT_CHAT_ID
 
 
 def send_telegram(message):
-    if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+
+    if not TELEGRAM_TOKEN or not ALERT_CHAT_ID:
         print("Telegram not configured properly")
         return
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
     payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
+        "chat_id": ALERT_CHAT_ID,
         "text": message
     }
 
     try:
-        response = requests.post(url, json=payload)
-
-        if response.status_code != 200:
-            print(f"Telegram failed: {response.text}")
-
+        requests.post(url, data=payload)
     except Exception as e:
-        print(f"Telegram error: {e}")
+        print(f"Telegram Error: {e}")
