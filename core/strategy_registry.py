81fcc57d3fd73ef04ea34858db2f0d491068a46d
Jonomuzz <156991284+Jonomuzz@users.noreply.github.com>
2026-05-17 14:11:12 +1000

    Add strategy functions to strategy registry

--- core/strategy_registry.py
+++ core/strategy_registry.py
@@ -1,1 +1,18 @@
+from strategies import (
+    mean_reversion,
+    trend_strength_crossover,
+    breakout,
+    momentum,
+    kst
+)
 
+
+def get_strategy(name):
+    mapping = {
+        "mean_reversion": mean_reversion,
+        "trend": trend_strength_crossover,
+        "breakout": breakout,
+        "momentum": momentum,
+        "kst": kst
+    }
+    return mapping.get(name)
