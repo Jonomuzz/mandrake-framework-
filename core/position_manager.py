c5f780c0cf5de71390650f50681078b12ebfbd17
Jonomuzz <156991284+Jonomuzz@users.noreply.github.com>
2026-05-17 14:10:09 +1000

    Implement position management functions

--- core/position_manager.py
+++ core/position_manager.py
@@ -1,1 +1,18 @@
+positions = {}
 
+
+def init(symbols):
+    for s in symbols:
+        positions[s] = None
+
+
+def open_position(symbol):
+    positions[symbol] = "LONG"
+
+
+def close_position(symbol):
+    positions[symbol] = None
+
+
+def is_open(symbol):
+    return positions[symbol] is not None
